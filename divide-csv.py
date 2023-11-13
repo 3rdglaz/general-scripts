5db8c36f8f46bb92fe15e0d8ef6b00a2d872cb26
Caio Pereira <pmscaio@gmail.com>
2023-11-13 09:47:08 -0300

    refactor: optimized code, just a few changes

--- divide-csv.py
+++ divide-csv.py
@@ -1,26 +1,27 @@
-input_filename = r'C:\Users\caio.silva\Downloads\17-08 Juiz de fora - Página2.csv'
-output_filename_pattern = r'C:\Users\caio.silva\Documentos\lista_dividida_'
-line_limit = 100
+import os
+
+input_filename = r"C:\CSV\07-11.csv"
+output_filename_pattern = r"venc_aprox_"
+line_limit = 150
 line = 0
 i = 0
 file_number = 0
 start = 0
 
 with open(input_filename, 'r') as input_file:
-    lines = input_file.readlines()
-    total_lines = len(lines)
+    lines = input_file.readlines() 
+    total_lines = len(lines) 
 
-    while line <= total_lines:
-        if i == line_limit or line == total_lines:
-            file_number += 1
+    while line <= total_lines: 
+        if  i == line_limit or line == total_lines:
+            file_number += 1 
             filename = f"{output_filename_pattern}{file_number}.csv"
-            with open(filename, 'w') as output_file:
-                # tá inserindo o "telefone" em todas, mas criando uma vazia
-                output_file.write("telefone\n")
-                output_file.writelines(lines[start:line])
-            start = line
-            i = 0
+            with open(filename, 'w') as output_file: 
+                output_file.write("telefone\n") 
+                output_file.writelines(lines[start:line]) 
+            start = line 
+            i = 0 
             print(filename)
         
-        i += 1
-        line += 1
+        i += 1 
+        line += 1 
